eb74ffee02619c568768b9eaadd2f13979a84831
fnowak <filip.nowak7@gmail.com>
2024-10-03 18:38:20 +0200

    zrobiony backend

--- app/controllers/EventController.ts
+++ app/controllers/EventController.ts
@@ -7,23 +7,36 @@ const router = express.Router();
 
 
 const EventController = {
-  index: (_req: Request, res: Response<EventsResponse>) => {
-    res.json({
-      events: [
-        {
-          name: "Filip",
-          event: { key: "front-end", val: "Front End" },
-          city: { key: "cracow", val: "Kraków" }
-        },
-        {
-          name: "Ali",
-          event: { key: "back-end", val: "Back End" },
-          city: { key: "warsaw", val: "Warszawa" }
-        },
-      ],
-    });
+  // index: (_req: Request, res: Response<EventsResponse>) => {
+  //   res.json({
+  //     events: [
+  //       {
+  //         name: "Filip",
+  //         event: { key: "front-end", val: "Front End" },
+  //         city: { key: "cracow", val: "Kraków" }
+  //       },
+  //       {
+  //         name: "Ali",
+  //         event: { key: "back-end", val: "Back End" },
+  //         city: { key: "warsaw", val: "Warszawa" }
+  //       },
+  //     ],
+  //   });
+  // },
+  index: (_req: Request, res: Response) => {
+    EventModel.find({})
+      .then((events) => {
+        res.status(200).json({
+          events: events, // Zwracanie wydarzeń jako odpowiedź
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "Error while fetching events",
+          error: err,
+        });
+      });
   },
-
   create: (req: Request, res: Response) => {
     const event = new EventModel({
       name: req.body.name,
@@ -46,27 +59,26 @@ const EventController = {
   delete: (req: Request, res: Response) => {
     const id = req.params.id;
   
-    // Typy dla err i event
-    EventModel.findByIdAndDelete(id, (err: Error | null, event: IEvent | null) => {
-      if (err) {
+    EventModel.findByIdAndDelete(id)
+      .then((event) => {
+        if (!event) {
+          return res.status(404).json({
+            message: "Event not found",
+          });
+        }
+          console.log(`Event with id ${id} deleted successfully`);
+  
+        return res.status(200).json({
+          id: id,
+          deleted: true,
+        });
+      })
+      .catch((err) => {
         return res.status(500).json({
           message: "Error while deleting event",
           error: err,
         });
-      }
-  
-      // Jeśli zdarzenie nie zostało znalezione
-      if (!event) {
-        return res.status(404).json({
-          message: "Event not found",
-        });
-      }
-  
-      return res.status(200).json({
-        id: id,
-        deleted: true,
       });
-    });
   }
 };
 
